34845aeb095f36dee052fa7cf9ff6c5556d67aee
les <6203>
2020-10-22 17:14:45 +0800

    mutool子项目建立

---
+++ mutool-doc/src/main/java/com/mutool/doc/MutoolDocApplication.java
@@ -0,0 +1,13 @@
+package com.mutool.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutoolDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutoolDocApplication.class, args);
+	}
+
+}
